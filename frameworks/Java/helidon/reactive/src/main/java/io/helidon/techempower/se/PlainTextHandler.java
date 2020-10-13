0dfd12e08748c28e2880ab9831abe81629d364da
Tomas Langer <tomas.langer@oracle.com>
2020-10-13 22:19:58 +0200

    Improvement on plain text.

--- frameworks/Java/helidon/reactive/src/main/java/io/helidon/techempower/se/PlainTextHandler.java
+++ frameworks/Java/helidon/reactive/src/main/java/io/helidon/techempower/se/PlainTextHandler.java
@@ -1,5 +1,10 @@
 package io.helidon.techempower.se;
 
+import java.nio.charset.StandardCharsets;
+
+import io.helidon.common.http.DataChunk;
+import io.helidon.common.http.MediaType;
+import io.helidon.common.reactive.Single;
 import io.helidon.webserver.Handler;
 import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
@@ -8,11 +13,14 @@ import io.helidon.webserver.ServerResponse;
  * Handler for the plain text test.
  */
 class PlainTextHandler implements Handler {
+    private static final byte[] MESSAGE = "Hello, World!".getBytes(StandardCharsets.UTF_8);
+
     PlainTextHandler() {
     }
 
     @Override
     public void accept(ServerRequest req, ServerResponse res) {
-        res.send("Hello, World!");
+        res.headers().contentType(MediaType.TEXT_PLAIN);
+        res.send(Single.just(DataChunk.create(MESSAGE)));
     }
 }
